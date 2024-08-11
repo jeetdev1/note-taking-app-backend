b2dc464b0cb06e65bc78676346473cb62c6f9167
Jeet Mukherjee <jeetmukherjee100@gmil.com>
2024-08-11 19:48:11 +0530

    add prettier and eslint

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,13 @@
 import dotenv from "dotenv";
 import express, {
-  ErrorRequestHandler,
   Express,
-  NextFunction,
   Request,
   Response,
 } from "express";
-import { ReasonPhrases, StatusCodes } from "http-status-codes";
 import morgan from "morgan";
 import { pool } from "./db";
 import { converErrors, handleError } from "./middlewares/error";
 import router from "./routes/index";
-import ApiError from "./utils/ApiError";
 dotenv.config({ path: ".env" });
 
 const app: Express = express();
@@ -19,10 +15,6 @@ const app: Express = express();
 app.use(morgan("tiny"));
 
 app.use(express.json());
-
-// pool.on("connect", () => {
-//   console.log("DB connected");
-// });
 
 pool.on("error", () => {
   console.log("DB disconnected");
